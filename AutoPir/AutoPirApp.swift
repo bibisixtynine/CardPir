1aed7813e6f1b1514bff23dd02adedc7a6cb7f55
Jérôme Binachon <ilboued@proton.me>
2023-07-27 08:19:18 +0200

    Initial Commit

---
+++ AutoPir/AutoPirApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoPirApp.swift
+//  AutoPir
+//
+//  Created by Jérôme Binachon on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AutoPirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
